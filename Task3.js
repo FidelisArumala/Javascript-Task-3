5b450f1f4df02b1dd1e34a1f4dd99f2466bb4094
FidelisArumala <60404832+FidelisArumala@users.noreply.github.com>
2020-04-19 13:51:13 +0100

    first commit

---
+++ Task3.js
@@ -0,0 +1,48 @@
+
+// function for whole number
+function wholeNumber(num){
+    let array=[];
+
+    // for loop
+    for(let i=1; i<=num; i++) {
+        let add=""
+
+        // conditional statements for arguments
+      if(i%5===1 ||i%3===1 || i%2===1){
+    if(i%2===1){
+        if(add!="")
+        add+='-yu' 
+        else{
+            add='yu' 
+        }
+    }
+     if(i%3===1){
+            if(add!="")
+            add+='-gi' 
+            else{
+                add='gi' 
+            }
+        }
+        if(i%5===1){
+            if(add!="")
+            add+='-oh' 
+            else{
+                add='oh' 
+            }
+        }
+}
+        else{
+            add=i
+        }
+
+        // array returns
+        array.push(add)
+    }
+    console.log(array);
+    
+    
+    
+}
+// calling the function
+wholeNumber(100)
+wholeNumber(55)
